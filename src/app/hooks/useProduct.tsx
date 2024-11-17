7980d24f094245a22b98dfdd5ad1846f43a58707
Pahmi Alifya Bahri <pahmi.alifya@efishery.com>
2024-11-17 13:34:08 +0700

    feat(hooks): add product hook

---
+++ src/app/hooks/useProduct.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { useState, useEffect } from "react"
+
+type Image = {
+  id: string[]
+  image: string
+}
+
+type Product = {
+  id: string
+  name: string
+  image?: string
+}
+
+export const useProduct = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState<Error | null>(null)
+  const [data, setData] = useState<Product[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [productsResponse, imagesResponse] = await Promise.all([
+          fetch("https://www.giovankov.com/api/product.json"),
+          fetch("https://www.giovankov.com/api/image.json"),
+        ])
+
+        if (!productsResponse.ok || !imagesResponse.ok) {
+          throw new Error(
+            `HTTP error! Product status: ${productsResponse.status}, Image status: ${imagesResponse.status}`
+          )
+        }
+
+        const productsData = await productsResponse.json()
+        const imagesData = await imagesResponse.json()
+
+        const combinedData = productsData.data.map((product: Product) => {
+          const image = imagesData.data.find((img: Image) =>
+            img.id.includes(product.id)
+          )?.image
+          return { ...product, image }
+        })
+        setData(combinedData)
+      } catch (error: any) {
+        setIsError(error)
+        console.error("Error fetching data:", error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return { isLoading, isError, data }
+}
